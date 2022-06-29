7adfb7fa62f982011efea1d1f0a6c4c8fef70cc3
Gabriel <gabrielproencaaraujo@gmail.com>
2022-06-29 19:35:47 -0300

    Item 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
-
+const getEmployeeByName = (employeeName) => {
+  const emp = data.employees;
+  if (employeeName === undefined) {
+    return {};
+  }
+  return emp.find((e) => e.firstName === employeeName || e.lastName === employeeName);
+};
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
