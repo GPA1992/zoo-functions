659d85dee1abc7890e2395010cc3e7ab860b4c5e
Gabriel <gabrielproencaaraujo@gmail.com>
2022-06-30 12:03:41 -0300

    item 4 - graças a deus pai - MELHOR DO MUNDO

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const isManager = (id) => managers.some((gerente) => gerente === id);
 
+const getRelatedEmployees = (managerId) => {
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const emp = data.employees;
+  const empByManager = emp.filter((e) => e.managers.some((p) => p === managerId));
+  return empByManager.map((person) => `${person.firstName} ${person.lastName}`);
+};
+console.log(getRelatedEmployees(olaId));
 module.exports = { isManager, getRelatedEmployees };
+
+//
+//
+//
+//
+//
+/* const getEmployeeByManager = (managerName) => {
+  const emp = data.employees;
+  return emp.filter((e) => e.managers.some((p) => p === managerName));
+};
+
+console.log(getEmployeeByManager(stephanieId)); */
