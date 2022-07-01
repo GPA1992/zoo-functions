cee2cb0b76e5f4801ed10083af7ba40255e1c273
Gabriel <gabrielproencaaraujo@gmail.com>
2022-06-30 22:45:38 -0300

    Item 5 - recebaaa

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    return data.species.reduce((acc, cur) => ({ ...acc, [cur.name]: cur.residents.length }), {});
+  }
+  if (!animal.sex) {
+    return data.species.find((a) => a.name === animal.specie).residents.length;
+  }
+  return data.species
+    .find((a) => a.name === animal.specie).residents.filter((b) => b.sex === animal.sex).length;
+};
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
