983068c3aaecf2da1cca2efe66d8754a48379f56
Gabriel <gabrielproencaaraujo@gmail.com>
2022-07-03 14:16:49 -0300

    Item 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const employeeList = data.employees;
+const specie = data.species;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employe = employeeList.filter((a) => a.id === id);
+  const firstAnimal = employe[0].responsibleFor[0];
+  const getSpecie = specie.filter((s) => s.id === firstAnimal)[0].residents;
+  const getOlder = Math.max(...getSpecie.map((older) => older.age));
+  const getOlderSpecie = getSpecie.filter((esp) => esp.age === getOlder);
+  return [getOlderSpecie[0].name, getOlderSpecie[0].sex, getOlderSpecie[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
